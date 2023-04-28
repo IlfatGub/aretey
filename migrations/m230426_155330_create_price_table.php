7e95c92667a0ae36972a988b9882f2bf3b67b83f
ilfat <ilfat.gubaidullin@gmail.com>
2023-04-28 22:59:23 +0500

    migrate

--- migrations/m230426_155330_create_price_table.php
+++ migrations/m230426_155330_create_price_table.php
@@ -16,38 +16,72 @@ class m230426_155330_create_price_table extends Migration
             'id' => $this->primaryKey(),
             'name' => $this->text()->notNull()->comment('Наименование услуги'),
             'category' => $this->string(255)->notNull()->comment('Категория'),
-            'code' => $this->string(255)->notNull()->comment('Код'),
+            'code' => $this->string(255)->notNull()->unique()->comment('Код'),
             'time' => $this->string(255)->defaultValue(null)->comment('Срок'),
             'price' => $this->smallInteger()->defaultValue(null)->comment('Цена'),
             'count' => $this->smallInteger()->defaultValue(null)->comment('Количество'),
             'deleted' => $this->smallInteger()->defaultValue(null)->comment('Удален'),
-            'id_type' => $this->smallInteger()->defaultValue(null)->comment('Тип услуги'),
-            'id_biom' => $this->smallInteger()->defaultValue(null)->comment('Биоматериал'),
+            'type' => $this->text()->notNull()->comment('Тип услуги'),
+            'biom' => $this->text()->notNull()->comment('Биоматериал'),
         ]);
 
 
         $this->insert('{{%price}}', [
-            'name'=>'Лейкоцитарная формула (микроскопия)',
-            'category'=>'6',
+            'name'=>'ПРОФИЛЬ Клинический (общий) анализ крови (CBC, 5-Diff) с микроскопией мазка крови при выявлении патологических изменений + СОЭ',
+            'category'=>'ГЕМАТОЛОГИЧЕСКИЕ ИССЛЕДОВАНИЯ',
             'code'=>'11-10-004',
             'time'=>'1 р. д.',
             'price'=>'185',
-            'id_type'=>'2',
-            'id_biom'=>'6',
+            'type'=>'колич',
+            'biom'=>'Венозная кровь',
             ]
         );
 
         $this->insert('{{%price}}', [
-            'name'=>'Тельца Гейнца',
-            'category'=>'7',
-            'code'=>'11-10-008',
-            'time'=>'2 р. д.',
-            'price'=>'200',
-            'id_type'=>'1',
-            'id_biom'=>'4',
+            'name'=>'Лейкоцитарная формула (микроскопия)',
+            'category'=>'ГЕМАТОЛОГИЧЕСКИЕ ИССЛЕДОВАНИЯ',
+            'code'=>'11-10-003',
+            'time'=>'1 р. д.',
+            'price'=>'185',
+            'type'=>'колич',
+            'biom'=>'Венозная кровь',
             ]
         );
 
+        $this->insert('{{%price}}', [
+            'name'=>'Осмотическая стойкость эритроцитов (анемии)',
+            'category'=>'ГЕМАТОЛОГИЧЕСКИЕ ИССЛЕДОВАНИЯ',
+            'code'=>'11-10-002',
+            'time'=>'1 р. д.',
+            'price'=>'185',
+            'type'=>'колич',
+            'biom'=>'Венозная кровь',
+            ]
+        );
+
+
+        $this->insert('{{%price}}', [
+            'name'=>'Аллоиммунные антиэритроцитарные антитела (в непрямой реакции Кумбса, включая антирезус Ат)',
+            'category'=>'ИММУНОГЕМАТОЛОГИЯ',
+            'code'=>'12-10-006',
+            'time'=>'1 р. д.',
+            'price'=>'185',
+            'type'=>'колич',
+            'biom'=>'Венозная кровь',
+            ]
+        );
+
+        $this->insert('{{%price}}', [
+            'name'=>'Антигены системы Kell',
+            'category'=>'ИММУНОГЕМАТОЛОГИЯ',
+            'code'=>'12-10-010',
+            'time'=>'1 р. д.',
+            'price'=>'185',
+            'type'=>'колич',
+            'biom'=>'Венозная кровь',
+            ]
+        );
+        
         echo shell_exec("php yii gii/model --tableName=price --modelClass=Prices --interactive=0 --overwrite=1 --ns=app\\models");
     }
 
