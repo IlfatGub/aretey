a6f45d81767527843322fc1877f620a58b07ed1d
ilfat <ilfat.gubaidullin@gmail.com>
2023-05-13 16:59:02 +0500

    serach patient

---
+++ models/PatientSearch.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Patient;
+
+/**
+ * PatientSearch represents the model behind the search form of `app\models\Patient`.
+ */
+class PatientSearch extends Patient
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'parent_id', 'brithday', 'visible'], 'integer'],
+            [['surname', 'name', 'patronymic', 'fullname', 'address_city', 'address_street', 'address_home', 'address_room', 'document', 'passport_serial', 'passport_number', 'passport_issued', 'phone'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Patient::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'parent_id' => $this->parent_id,
+            'brithday' => $this->brithday,
+            'visible' => $this->visible,
+        ]);
+
+        $query->andFilterWhere(['like', 'surname', $this->surname])
+            ->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'patronymic', $this->patronymic])
+            ->andFilterWhere(['like', 'fullname', $this->fullname])
+            ->andFilterWhere(['like', 'address_city', $this->address_city])
+            ->andFilterWhere(['like', 'address_street', $this->address_street])
+            ->andFilterWhere(['like', 'address_home', $this->address_home])
+            ->andFilterWhere(['like', 'address_room', $this->address_room])
+            ->andFilterWhere(['like', 'document', $this->document])
+            ->andFilterWhere(['like', 'passport_serial', $this->passport_serial])
+            ->andFilterWhere(['like', 'passport_number', $this->passport_number])
+            ->andFilterWhere(['like', 'passport_issued', $this->passport_issued])
+            ->andFilterWhere(['like', 'phone', $this->phone]);
+
+        return $dataProvider;
+    }
+}
