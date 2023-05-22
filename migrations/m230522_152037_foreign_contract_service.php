80cf690f480b13e8106ee1d7de1808efea7a6c83
ilfat <ilfat.gubaidullin@gmail.com>
2023-05-22 20:25:51 +0500

    migratef foreign

---
+++ migrations/m230522_152037_foreign_contract_service.php
@@ -0,0 +1,58 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230522_152037_foreign_contract_service
+ */
+class m230522_152037_foreign_contract_service extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addForeignKey(
+            'fk-price-service',
+            'price',
+            'id',
+            'contract_service',
+            'id_service',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk-contract-service',
+            'contract',
+            'id',
+            'contract_service',
+            'id_contract',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m230522_152037_foreign_contract_service cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230522_152037_foreign_contract_service cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
