42f5bff783f5b29679b8215cfbae0eeebfb89152
ilfat <ilfat.gubaidullin@gmail.com>
2023-05-31 17:07:45 +0500

    placeholder & redirect

--- views/patient/_form.php
+++ views/patient/_form.php
@@ -23,7 +23,7 @@ $document = $model->arrayFilter($patient, 'document');
 $passport_issued = $model->arrayFilter($patient, 'passport_issued');
 $name = $model->arrayFilter($patient, 'name');
 $surname = $model->arrayFilter($patient, 'surname');
-$patronymic = $model->arrayFilter($patient, 'patrronymic');
+$patronymic = $model->arrayFilter($patient, 'patronymic');
 ?>
 
 <div class="patient-form <?= $ajax ? 'fs-8' : '' ?>">
@@ -36,58 +36,56 @@ $patronymic = $model->arrayFilter($patient, 'patrronymic');
 
     <div class="row">
         <div class="col">
-            <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'surname', 'local' => $surname, 'placeholder' => 'Имя']) ?>
+            <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'surname', 'local' => $surname, 'placeholder' => 'Фамилия']) ?>
         </div>
         <div class="col">
-            <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'name', 'local' => $name, 'placeholder' => 'Фамилия']) ?>
+            <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'name', 'local' => $name, 'placeholder' => 'Имя']) ?>
         </div>
         <div class="col">
             <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'patronymic', 'local' => $patronymic, 'placeholder' => 'Отчество']) ?>
         </div>
         <div class="col">
-            <?= $form->field($model, 'phone')->textInput(['maxlength' => true, 'class' => 'form-control ', 'placeholder' => 'Телефон'])->label() ?>
+            <?= $form->field($model, 'phone')->textInput(['maxlength' => true, 'class' => 'form-control', 'autocomplete' => 'off', 'placeholder' => 'Телефон'])->label() ?>
         </div>
         <div class="col">
             <?php
             // Usage with model and Active Form (with no default initial value)
             echo $form->field($model, 'brithday')->widget(DatePicker::classname(), [
-                'options' => ['placeholder' => 'Дата рождения'],
+                'options' => ['placeholder' => 'Дата рождения', 'autocomplete' => 'off',],
                 'size' => 'sm',
                 'pluginOptions' => [
                     'autoclose' => true,
-                    'format' => 'yyyy-mm-dd'
+                    'format' => 'yyyy-mm-dd',
                 ],
             ])->label();
             ?>
         </div>
     </div>
 
-    <?php if ($type <> 2) : ?>
-        <div class="row">
-            <div class="col-4">
-                <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'address_city', 'local' => $city, 'placeholder' => 'Населенный пункт']) ?>
-            </div>
-            <div class="col-4">
-                <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'address_street', 'local' => $street, 'placeholder' => 'Улица']) ?>
-            </div>
-            <div class="col-2">
-                <?= $form->field($model, 'address_home')->textInput(['maxlength' => true, 'class' => 'form-control', 'placeholder' => 'Дом'])->label() ?>
-            </div>
-            <div class="col-2">
-                <?= $form->field($model, 'address_room')->textInput(['maxlength' => true, 'class' => 'form-control', 'placeholder' => 'Квартира'])->label() ?>
-            </div>
+    <div class="row">
+        <div class="col-4">
+            <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'address_city', 'local' => $city, 'placeholder' => 'Населенный пункт']) ?>
         </div>
-    <?php endif; ?>
+        <div class="col-4">
+            <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'address_street', 'local' => $street, 'placeholder' => 'Улица']) ?>
+        </div>
+        <div class="col-2">
+            <?= $form->field($model, 'address_home')->textInput(['maxlength' => true, 'class' => 'form-control', 'autocomplete' => 'off', 'placeholder' => 'Дом'])->label() ?>
+        </div>
+        <div class="col-2">
+            <?= $form->field($model, 'address_room')->textInput(['maxlength' => true, 'class' => 'form-control', 'autocomplete' => 'off', 'placeholder' => 'Квартира'])->label() ?>
+        </div>
+    </div>
 
     <div class="row">
         <div class="col-3">
             <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'document', 'local' => $document, 'placeholder' => 'Документ']) ?>
         </div>
         <div class="col">
-            <?= $form->field($model, 'passport_serial')->textInput(['maxlength' => true, 'class' => 'form-control', 'placeholder' => 'Серия'])->label() ?>
+            <?= $form->field($model, 'passport_serial')->textInput(['maxlength' => true, 'class' => 'form-control', 'autocomplete' => 'off', 'placeholder' => 'Серия'])->label() ?>
         </div>
         <div class="col">
-            <?= $form->field($model, 'passport_number')->textInput(['maxlength' => true, 'class' => 'form-control', 'placeholder' => 'Номер'])->label() ?>
+            <?= $form->field($model, 'passport_number')->textInput(['maxlength' => true, 'class' => 'form-control', 'autocomplete' => 'off',  'placeholder' => 'Номер'])->label() ?>
         </div>
         <div class="col-5">
             <?= TypeheadWidget::widget(['form' => $form, 'model' => $model, 'field' => 'passport_issued', 'local' => $passport_issued, 'placeholder' => 'Когда, кем выдано']) ?>
@@ -98,12 +96,13 @@ $patronymic = $model->arrayFilter($patient, 'patrronymic');
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
 
         <?php if (Yii::$app->controller->action->id == 'update') : ?>
-            <?=
+        <?=
             Html::a('Удалить', ['delete', 'id' => $_GET['id']], [
                 'class' => 'btn btn-danger',
                 'data' => [
                     'confirm' => 'Вы уверены, что хотите удалить этот элемент?',
                     'method' => 'post',
+                    'autocomplete' => 'off',
                 ],
             ])
             ?>
