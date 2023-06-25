43b7952e521ccd9720b5e36f1ba4b58e2b8920b6
ilfat <ilfat.gubaidullin@gmail.com>
2023-06-25 20:16:26 +0500

    download button contract form

--- views/contract/_form.php
+++ views/contract/_form.php
@@ -112,7 +112,7 @@ $patient_list = ArrayHelper::map(Patient::find()->orderBy(['fullname' => SORT_AS
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
 
         <?php if (Yii::$app->controller->action->id == 'update') : ?>
-        <?=
+            <?=
             Html::a('Удалить', ['delete', 'id' => $_GET['id']], [
                 'class' => 'btn btn-danger',
                 'data' => [
@@ -122,6 +122,21 @@ $patient_list = ArrayHelper::map(Patient::find()->orderBy(['fullname' => SORT_AS
                 ],
             ])
             ?>
+            <div class="dropdown float-right">
+                <a class="btn btn-lg btn-secondary dropdown-toggle fs-8" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="text-decoration: none !important;">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-word" viewBox="0 0 16 16" style="color:white ">
+                            <path d="M5.485 6.879a.5.5 0 1 0-.97.242l1.5 6a.5.5 0 0 0 .967.01L8 9.402l1.018 3.73a.5.5 0 0 0 .967-.01l1.5-6a.5.5 0 0 0-.97-.242l-1.036 4.144-.997-3.655a.5.5 0 0 0-.964 0l-.997 3.655L5.485 6.88z" />
+                            <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z" />
+                        </svg>
+                    </span>
+                </a>
+                <ul class="dropdown-menu fs-10">
+                    <li><a class="dropdown-item" href="<?= Url::toRoute(['contract/download', 'id' => $id, 'type' => 1]) ?>">Взрослый договор платных услуг</a></li>
+                    <li><a class="dropdown-item" href="<?= Url::toRoute(['contract/download', 'id' => $id, 'type' => 2]) ?>">Детский договор платных услуг</a></li>
+                    <li><a class="dropdown-item" href="<?= Url::toRoute(['contract/download', 'id' => $id, 'type' => 3]) ?>">Согласие для детей</a></li>
+                </ul>
+            </div>
         <?php endif; ?>
     </div>
     <?php ActiveForm::end(); ?>
